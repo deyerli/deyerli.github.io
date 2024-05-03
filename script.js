d7c016846aec9e6cd623e549856e6910cc858e5b
deyerli <70439245+deyerli@users.noreply.github.com>
2024-05-03 14:36:27 -0400

    first messagge

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript code can go here for interactive features
-
-// Example: Change background color on button click
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'lightgray';
-});
